7e9349e8cfad6c63b10f0508fec61c4feb8f7095
Abhijith <abhijithaips@gmail.com>
2025-10-02 18:40:13 +0530

    Feature: Add password recovery functionality

--- quantum/settings.py
+++ quantum/settings.py
@@ -121,3 +121,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+# --- EMAIL SETTINGS FOR PASSWORD RESET ---
+# For development, we print emails to the console.
+# For production, you would use a real email service like SendGrid or Mailgun.
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
